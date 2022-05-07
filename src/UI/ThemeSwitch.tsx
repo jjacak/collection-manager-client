dab461bab400b920a72531e8e98e6feb7f6e4d3d
Joanna Jacak <joanna.jacak@protonmail.com>
2022-05-07 20:23:46 +0200

    fixed issues with theme switch

--- src/UI/ThemeSwitch.tsx
+++ src/UI/ThemeSwitch.tsx
@@ -1,18 +1,28 @@
 import { Form } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
+
 
 const ThemeSwitch: React.FC<{ onClick: () => void; theme: string }> = (
 	props
 ) => {
+	const defaultCheckedState = props.theme==='dark'?true:false;
+	const [checked, setChecked]= useState<boolean>(defaultCheckedState)
 	const { t } = useTranslation();
+
+	const switchHandler = ()=>{
+		setChecked(previous=>!previous)
+	}
 	return (
 		<Form>
 			<Form.Check
 				type="switch"
 				id="theme-switch"
 				label={t('dark_theme')}
-				checked={props.theme === 'dark' ? true : false}
+				checked={checked}
 				onClick={props.onClick}
+				onChange = {switchHandler}
+			
 			/>
 		</Form>
 	);
