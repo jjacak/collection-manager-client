c883fb8663a9716023066729c1cfd4f4150d0884
Joanna Jacak <joanna.jacak@protonmail.com>
2022-06-02 23:18:00 +0200

    delete item function

--- src/pages/ViewItem.tsx
+++ src/pages/ViewItem.tsx
@@ -1,24 +1,29 @@
-import { useParams, NavLink } from 'react-router-dom';
+import { useParams, NavLink, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { CollectionInterface, CollectionItem } from '../ts/types';
+import { Button } from 'react-bootstrap';
+import useHttp from '../hooks/use-http';
 
 const ViewItem = () => {
 	const { id } = useParams();
+	const navigate = useNavigate()
 	const [collection, setCollection] = useState<CollectionInterface | null>(
 		null
 	);
 	const { t } = useTranslation();
-	const { user } = useAuth0();
-
-	const item:CollectionItem|undefined = collection?.items?.filter((i) => i._id === id)[0];
+	const { user, getAccessTokenSilently } = useAuth0();
+	const { sendRequest: sendDeleteRequest } = useHttp();
+	const item: CollectionItem | undefined = collection?.items?.filter(
+		(i) => i._id === id
+	)[0];
 	const isOwner = user?.sub === collection?.owner_id;
 	const isAdmin =
 		user?.['http:/collection-manager-app.com/roles']?.includes('admin');
 	const isAuthorized = isOwner || isAdmin;
-
+	
 	useEffect(() => {
 		const getCollection = async () => {
 			const response = await axios.get(
@@ -29,18 +34,48 @@ const ViewItem = () => {
 		getCollection();
 	}, [id]);
 
+	const deleteItem = async () => {
+		const getResponse = (response: any) => {
+			navigate(`/view-collection/${collection?._id}`)
+		};
+
+		const accessToken = await getAccessTokenSilently({
+			audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+		});
+		sendDeleteRequest(`${process.env.REACT_APP_SERVER}/delete-item/${id}`, {
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+			},
+			method: 'DELETE',
+		}, getResponse);
+	};
+
 	const itemAttributes = item ? Object.entries(item) : [];
-	const itemValues = <div>
-		{itemAttributes.map((i) => 
-			{if(['name', 'tags', 'date', 'author', '_id', 'likes', 'comments'].includes(i[0])){
-				return
-			}
-		return <p className="fs-4" key={i[0]}>
-			<span className="fw-bold">{i[1].label}: </span>
-			{i[1].value}
-		</p>}
-	)}
-	</div>
+	const itemValues = (
+		<div>
+			{itemAttributes.map((i) => {
+				if (
+					[
+						'name',
+						'tags',
+						'date',
+						'author',
+						'_id',
+						'likes',
+						'comments',
+					].includes(i[0])
+				) {
+					return;
+				}
+				return (
+					<p className="fs-4" key={i[0]}>
+						<span className="fw-bold">{i[1].label}: </span>
+						{i[1].value}
+					</p>
+				);
+			})}
+		</div>
+	);
 
 	return (
 		<section>
@@ -57,16 +92,20 @@ const ViewItem = () => {
 					{t('return')}
 				</NavLink>
 				{isAuthorized && (
-					<NavLink
-						className="btn"
-						style={{
-							borderColor: 'var(--accent)',
-							color: 'var(--text-primary',
-						}}
-						to={`/add-item/${id}`}
-					>
-						{t('edit')}
-					</NavLink>
+					<>
+						<NavLink
+							className="btn"
+							style={{
+								borderColor: 'var(--accent)',
+								color: 'var(--text-primary',
+								marginRight: '3px',
+							}}
+							to={`/add-item/${id}`}
+						>
+							{t('edit')}
+						</NavLink>
+						<Button className="btn-danger" onClick={deleteItem}>{t('delete')}</Button>
+					</>
 				)}
 			</div>
 			<article>
